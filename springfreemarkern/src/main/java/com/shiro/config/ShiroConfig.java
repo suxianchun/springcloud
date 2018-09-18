5da29819f3b01808826768e97d32590f4cad8069
x.su <415858791@qq.com>
2018-09-18 13:35:26 +0800

    first

---
+++ springfreemarkern/src/main/java/com/shiro/config/ShiroConfig.java
@@ -0,0 +1,12 @@
+package com.shiro.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by x.su on 2018-09-18.
+ */
+@Configuration
+public class ShiroConfig {
+
+
+}
---
+++ springfreemarkern/src/main/java/com/shiro/session/ShiroSessionManager.java
@@ -0,0 +1,41 @@
+package com.shiro.session;
+
+
+import com.alibaba.druid.util.StringUtils;
+import org.apache.shiro.web.servlet.ShiroHttpServletRequest;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.apache.shiro.web.util.WebUtils;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.Serializable;
+
+/**
+ * Created by x.su on 2018-09-18.
+ */
+public class ShiroSessionManager extends DefaultWebSessionManager {
+
+    private static final String AUTHORIZATION = "Authorization";
+
+    private static final String REFERENCED_SESSION_ID_SOURCE = "Stateless request";
+
+    public ShiroSessionManager()
+    {
+        super();
+    }
+
+    @Override
+    protected Serializable getSessionId(ServletRequest request, ServletResponse response) {
+        String id = WebUtils.toHttp(request).getHeader(AUTHORIZATION);
+        if(!StringUtils.isEmpty(id)){
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_SOURCE, REFERENCED_SESSION_ID_SOURCE);
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID, id);
+            request.setAttribute(ShiroHttpServletRequest.REFERENCED_SESSION_ID_IS_VALID, Boolean.TRUE);
+            return id;
+        }else
+        {
+            return super.getSessionId(request, response);
+        }
+
+    }
+}
